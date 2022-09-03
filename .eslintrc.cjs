b5813b402e07e0c88b241f17bcf41f08046d800e
司文峰 <swf@192.168.1.2>
2022-09-03 16:23:36 +0800

    fix: test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@
  * @Description:
  * @Date: 2022-09-03 15:38:11
  * @LastEditors: siwenfeng
- * @LastEditTime: 2022-09-03 16:02:15
+ * @LastEditTime: 2022-09-03 16:21:03
  */
 module.exports = {
   env: {
@@ -17,9 +17,10 @@ module.exports = {
     'plugin:prettier/recommended'
   ],
   overrides: [],
-  parser: '@typescript-eslint/parser',
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint', 'prettier'],
@@ -27,24 +28,6 @@ module.exports = {
     'prettier/prettier': 'error',
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'upd',
-        'del',
-        'fix',
-        'refactor',
-        'test',
-        'perf',
-        'docs',
-        'style',
-        'revert'
-      ]
-    ],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never']
+    '@typescript-eslint/ban-types': 'off'
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@
  * @Description:
  * @Date: 2022-09-03 15:34:43
  * @LastEditors: siwenfeng
- * @LastEditTime: 2022-09-03 15:50:12
+ * @LastEditTime: 2022-09-03 16:10:04
  */
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app');
-
+createApp(App).mount('#app')
