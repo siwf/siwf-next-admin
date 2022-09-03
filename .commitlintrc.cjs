227fd4d7dccea7c4c1e96b316ba62d16f8c0f1b5
司文峰 <swf@192.168.1.2>
2022-09-03 16:02:57 +0800

    fix: init

---
+++ .commitlintrc.cjs
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @Date: 2022-09-03 15:56:14
+ * @LastEditors: siwenfeng
+ * @LastEditTime: 2022-09-03 15:56:28
+ */
+module.exports = {
+  extends: ["@commitlint/config-conventional"]
+}
--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,36 +2,49 @@
  * @Description:
  * @Date: 2022-09-03 15:38:11
  * @LastEditors: siwenfeng
- * @LastEditTime: 2022-09-03 15:47:52
+ * @LastEditTime: 2022-09-03 16:02:15
  */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended"
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'upd',
+        'del',
+        'fix',
+        'refactor',
+        'test',
+        'perf',
+        'docs',
+        'style',
+        'revert'
+      ]
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint",
-        "prettier"
-    ],
-    "rules": {
-        "prettier/prettier": "error",
-        "arrow-body-style": "off",
-        "prefer-arrow-callback": "off",
-        "@typescript-eslint/ban-types": 'off',
-    }
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never']
+  }
 }
